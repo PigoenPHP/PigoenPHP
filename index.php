17ecd63544ec9be6614648e66d4e6ee858623861
CodeWithSushil <sushilkumbhar77@gmail.com>
2024-03-23 13:43:21 +0530

    update

--- index.php
+++ index.php
@@ -1,19 +1,5 @@
 <?php
-<<<<<<< HEAD
 define("APP_ROOT", __DIR__);
-
-require_once APP_ROOT."/vendor/autoload.php";
-
-
-=======
-define('APP_ROOT', __DIR__);
-use PigoenPHP\PigoenPHP;
-
-require_once APP_ROOT."/vendor/autoload.php";
-
-$pigoen = new PigoenPHP();
-echo("<pre>");
-print_r($pigoen);
-// $pigoen->getMessage();
-$pigoen-setMessage("Hello Sushil");
->>>>>>> dev
+$t = require_once APP_ROOT."/vendor/autoload.php";
+echo "<pre>";
+Print_r($t);
