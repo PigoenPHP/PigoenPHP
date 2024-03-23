79fc61963284e71f32c736f04e72a4fd1fd0712c
CodeWithSushil <sushilkumbhar77@gmail.com>
2024-03-23 15:17:41 +0530

    update

--- Config/Database.php
+++ Config/Database.php
@@ -1,14 +1,19 @@
 <?php
-namespace Config;
-use Env;
+//namespace Config;
+//use Env;
+require "../vendor/autoload.php";
 
-class Database
-{
-  private string $host = "localhost:";
-  private string $user = "";
-  private string $pass = "";
-  private string $dbname = "";
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
 
-  echo "<pre>";
-  print_r($_ENV);
-}
+//class Database
+//{
+  // private string $host = "localhost:";
+  // private string $user = "";
+  // private string $pass = "";
+  // private string $dbname = "";
+
+
+$DB_PATH = $_ENV['path'];
+
+// }
