7e6afc0890b6a16bf26394f6f3dcec8de4275114
Sushil Kumar <sushilkumbhar77@gmail.com>
2024-08-24 22:45:58 +0530

    update

--- index.php
+++ index.php
@@ -1,18 +1,7 @@
 <?php
-define("APP_ROOT", __DIR__);
+define("APP_ROOT", __dir__);
 
-//require_once APP_ROOT."/vendor/autoload.php";
-require_once "./app/Pigoen.php";
-
-if($_SERVER['REQUEST_URI'] == "/" && $_SERVER['REQUEST_METHOD'] == "GET"){
-  $user = new PigoenPHP();
-  $user->setMessage("This is Text Message.");
-} elseif($_SERVER['REQUEST_URI'] == "/home" && $_SERVER['REQUEST_METHOD'] == "GET"){
-  require_once "./public/home.php";
-} else {
-  echo "404 - Page not Found.";
-}
-
-?>
-
-<link rel="stylesheet" href="https://www.jsdelivr.com/package/npm/bulma">
+require_once APP_ROOT ."/vendor/autoload.php";
+echo APP_ROOT;
+echo "<pre>";
+print_r($_SERVER);
