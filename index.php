9e636615b220429b629cf920238bed5e8c542504
Sushil Kumar <sushilkumbhar77@gmail.com>
2024-10-16 01:14:58 +0530

    Messaging App 2.0

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="./public/assets/images/favicon.ico" />
     <title>PigoenPHP</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
   </head>
