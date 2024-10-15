65533111e46fa0a842c3abef2fbecc8fc606d2ad
Sushil Kumar <sushilkumbhar77@gmail.com>
2024-10-15 23:49:55 +0530

    update

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
-<link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css"
->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PigoenPHP</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
+  </head>
+  <body>
+
+    <nav>
+      
+    </nav>
+
+    <main>
+      
+    </main>
+
+    <footer>
+      
+    </footer>
+
+    <script src="./js/index.js"></script>
+  </body>
+</html>
