212754eb05a468c717f68e949c418cae951b8d0a
Sushil Kumar <sushilkumbhar77@gmail.com>
2024-12-09 17:49:55 +0530

    new start

--- tests/Unit/ExampleTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-<<<<<<< HEAD
-test('example', function () {
-=======
-test('that true is true', function () {
->>>>>>> a847d22 (Move to Laravel and Reverb)
-    expect(true)->toBeTrue();
-});
